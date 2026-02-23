eb73264a39684449a0d7017be35aea8b2037c721
Paride Legovini <paride@ubuntu.com>
2026-02-23 17:46:57 +0100

    janitor: add missing hooks, and reorder existing ones

--- charms/autopkgtest-janitor-operator/src/charm.py
+++ charms/autopkgtest-janitor-operator/src/charm.py
@@ -31,17 +31,23 @@ class AutopkgtestJanitorCharm(ops.CharmBase):
             amqp_password=None,
         )
 
+        # basic hooks
         framework.observe(self.on.install, self._on_install)
+        framework.observe(self.on.config_changed, self._on_config_changed)
         framework.observe(self.on.start, self._on_start)
         framework.observe(self.on.upgrade_charm, self._on_install)
 
+        # action hooks
         framework.observe(self.on.add_remote_action, self._on_add_remote)
         framework.observe(self.on.remove_remote_action, self._on_remove_remote)
         framework.observe(
             self.on.rebuild_all_images_action, self._on_rebuild_all_images
         )
 
-        framework.observe(self.on.config_changed, self._on_config_changed)
+        # relation hooks
+        framework.observe(self.on.amqp_relation_joined, self._on_amqp_relation_joined)
+        framework.observe(self.on.amqp_relation_changed, self._on_amqp_relation_changed)
+        framework.observe(self.on.amqp_relation_broken, self._on_amqp_relation_broken)
 
     def _on_install(self, event: ops.InstallEvent):
         self.unit.status = ops.MaintenanceStatus("installing janitor charm")
