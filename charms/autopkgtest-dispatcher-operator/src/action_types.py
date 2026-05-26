8fd557107299071896cf944628b3a2666506cddf
ural.tunaboyu@canonical.com <ural.tunaboyu@canonical.com>
2026-05-26 09:03:56 -0700

    dispatcher: fixes for multiple remotes per arch
    Change systemd_helper and the set_worker_count actions to work with
    having multiple remotes per arch.
    
    Also change how remote names are passed to the worker as well as the key
    used for internal bookkeeping of workers per remote.

--- charms/autopkgtest-dispatcher-operator/src/action_types.py
+++ charms/autopkgtest-dispatcher-operator/src/action_types.py
@@ -29,4 +29,5 @@ class RemoveRemoteAction(pydantic.BaseModel):
 
 class SetWorkerCountAction(pydantic.BaseModel):
     arch: SupportedArches = pydantic.Field(description="Architecture to configure.")
+    index: int = pydantic.Field(description="Index of the remote to configure.")
     count: int = pydantic.Field(10)
