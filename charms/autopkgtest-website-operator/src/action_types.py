efb72c50bfa189d311c55839f20c55b414728762
Ural Tunaboyu <ural.tunaboyu@canonical.com>
2026-06-04 13:06:18 -0700

    website: expand alerts feature
    Significantly expand the alerts feature:
     - Allow setting multiple alerts at once.
     - Change remove-alert to take an index of which alert to remove.
     - Add new actions clear-all-alerts and list-alerts.
     - Change alerts file format to JSON.
     - Add datestamps to alerts.

--- charms/autopkgtest-website-operator/src/action_types.py
+++ charms/autopkgtest-website-operator/src/action_types.py
@@ -9,8 +9,12 @@ class AlertLevels(enum.Enum):
     DANGER = "danger"
 
 
-class SetAlertAction(pydantic.BaseModel):
+class AddAlertAction(pydantic.BaseModel):
     level: AlertLevels = pydantic.Field(
         description="Level of the alert. (info, warning, danger)"
     )
     message: str = pydantic.Field(description="The alert message to display.")
+
+
+class RemoveAlertAction(pydantic.BaseModel):
+    alert_id: int = pydantic.Field(description="ID of the alert to remove.")
